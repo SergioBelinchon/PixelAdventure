55f9656aeb5ecc37149a0f9f61d4a9dc9f3250cc
sergio belinchon <sergiorayo12@gmail.com>
2024-03-11 08:31:07 +0100

    PRIMER COMMIT

---
+++ lib/pixel_adventure.dart
@@ -0,0 +1,145 @@
+import 'dart:async';
+import 'package:flame/components.dart';
+import 'package:flame/events.dart';
+import 'package:flame/game.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:pixeladventure/components/jump_button.dart';
+import 'package:pixeladventure/components/level.dart';
+
+import 'components/hud.dart';
+import 'components/player1.dart';
+import 'components/player2.dart';
+
+class PixelAdventure extends FlameGame
+    with
+        HasKeyboardHandlerComponents,
+        DragCallbacks,
+        HasCollisionDetection,
+        TapCallbacks
+
+{
+  int health = 3;
+  late CameraComponent camara;
+  Player1 player1 = Player1(character: 'Pink Man');
+  Player2 player2 = Player2(character: 'Mask Dude');
+  late JoystickComponent joystick;
+  bool showControls = false;
+  bool playSounds = false;
+  double soundVolume = 1.0;
+  List <String> levelNames = ['Level05'];
+  int currentLevelIndex = 0;
+
+
+
+  @override
+  FutureOr<void> onLoad() async
+  {
+
+    //Carga todas las imagenes del cache
+    await images.loadAllImages();
+
+    _loadLevel();
+
+
+    if (showControls)
+    {
+      addJoystick();
+      add(JumpButton());
+    }
+    return super.onLoad();
+  }
+
+  @override
+  void update(double dt)
+  {
+
+    if (showControls)
+    {
+      updateJoystick();
+    }
+    return super.update(dt);
+  }
+
+  void addJoystick()
+  {
+    joystick = JoystickComponent(
+      priority: 10,
+      knob: SpriteComponent(
+        sprite: Sprite(
+          images.fromCache('HUD/Puntero.png'),
+        ),
+      ),
+      background: SpriteComponent(
+        sprite: Sprite(
+          images.fromCache('HUD/Joystick.png'),
+        ),
+      ),
+      margin: const EdgeInsets.only(left: 32, bottom: 32),
+    );
+    add(joystick);
+  }
+
+  void updateJoystick()
+  {
+    switch (joystick.direction)
+    {
+      case JoystickDirection.left:
+      case JoystickDirection.upLeft:
+      case JoystickDirection.downLeft:
+        player1.horizontalMovement = -1;
+        player2.horizontalMovement = -1;
+        break;
+      case JoystickDirection.right:
+      case JoystickDirection.upRight:
+      case JoystickDirection.downRight:
+        player1.horizontalMovement = 1;
+        player2.horizontalMovement = 1;
+        break;
+      default:
+        player1.horizontalMovement = 0;
+        player2.horizontalMovement = 0;
+        //Inactivo
+        break;
+    }
+  }
+
+  void loadNextLevel()
+  {
+    if(currentLevelIndex < levelNames.length - 1)
+      {
+        currentLevelIndex++;
+        _loadLevel();
+      }
+    else
+      {
+        // no mas niveles
+        currentLevelIndex = 0;
+        _loadLevel();
+      }
+  }
+
+  void _loadLevel()
+  {
+    Future.delayed(const Duration(seconds: 1,)
+    , ()
+    async {
+      Level world = Level(
+        player1: player1,
+        player2: player2,
+        levelName: levelNames[currentLevelIndex],
+      );
+
+      camara = CameraComponent.withFixedResolution(
+        world: world,
+        width: 640,
+        height: 360,
+      );
+      camara.viewfinder.anchor = Anchor.topLeft;
+
+      camara.viewport.add(Hud());
+
+      addAll([camara, world]);
+    });
+
+  }
+}
